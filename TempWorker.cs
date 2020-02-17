6df485fdda4f8e27121cb01a75fd5e84d5548d9b
Micheal Colhoun <micheal@cts-ni.com>
2020-02-17 20:57:37 +0000

    updated starting position of earth. Added checks for cpu temp not available turn off specific linux build flag

--- TempWorker.cs
+++ TempWorker.cs
@@ -1,61 +1,63 @@
 using System;
-using System.Diagnostics;
 using System.ComponentModel;
+using System.Diagnostics;
 
-namespace globe_webapi
-{
+namespace globe_webapi {
 
-public class TempWorker
-{
-    public CpuTemp  GetTempFromProcess()
-    {
-    
-        using (Process process = new Process())
-	{
-	    var cpu_temp = new CpuTemp();
-	    
-	    process.StartInfo.FileName = "sensors";
-	    process.StartInfo.Arguments = "-u"; 
-	    process.StartInfo.UseShellExecute = false;
-	    process.StartInfo.RedirectStandardOutput = true;
-	    process.StartInfo.RedirectStandardError = true;
-	    process.Start();
-	
-	    string line;
+	public class TempWorker {
+		static bool enabled = true;
 
-            while ((line = process.StandardOutput.ReadLine()) != null)
-	    {
-	        if  (line.Contains("temp3_input")) 
-	        {
-	            cpu_temp.cpu1_temp  = line.Split(':')[1]?.Trim();
-		    
-	        }
-		
-		if  (line.Contains("temp2_input")) 
-	        {
-	            cpu_temp.cpu0_temp  = line.Split(':')[1]?.Trim();
-		    
-	        }
-		
-		if  (line.Contains("temp1_input")) 
-	        {
-	            cpu_temp.mb_temp  = line.Split(':')[1]?.Trim();
-		    
-	        }
-            }
-	
-	    process.StandardOutput.ReadToEnd(); // ignore
-	    string err = process.StandardError.ReadToEnd();
-	    Console.WriteLine(err);
-	    process.WaitForExit();
-	    
-            return cpu_temp;
+		public CpuTemp GetTempFromProcess () {
+			var cpu_temp = new CpuTemp ();
+
+			if (enabled) {
+				using (Process process = new Process ()) {
+
+					process.StartInfo.FileName = "sensors";
+					process.StartInfo.Arguments = "-u";
+					process.StartInfo.UseShellExecute = false;
+					process.StartInfo.RedirectStandardOutput = true;
+					process.StartInfo.RedirectStandardError = true;
+
+					try {
+
+						process.Start ();
+
+						string line;
+
+						while ((line = process.StandardOutput.ReadLine ()) != null) {
+							if (line.Contains ("temp3_input")) {
+								cpu_temp.cpu1_temp = line.Split (':') [1]?.Trim ();
+
+							}
+
+							if (line.Contains ("temp2_input")) {
+								cpu_temp.cpu0_temp = line.Split (':') [1]?.Trim ();
+
+							}
+
+							if (line.Contains ("temp1_input")) {
+								cpu_temp.mb_temp = line.Split (':') [1]?.Trim ();
+
+							}
+						}
+
+						process.StandardOutput.ReadToEnd (); // ignore
+						string err = process.StandardError.ReadToEnd ();
+						Console.WriteLine (err);
+						process.WaitForExit ();
+					} catch (System.ComponentModel.Win32Exception) {
+						Console.WriteLine ($"new Process Failed. Is {process.StartInfo.FileName} Installed?");
+						enabled = false;
+					}
+				}
+
+			}
+
+			return cpu_temp;
+
+		}
+
 	}
-	    
-
-	
-    }    
-    
-}
 
 }
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 sudo systemctl stop kestrel-globe
-dotnet publish --configuration Release -o /var/www/globe/ --self-contained true -r linux-x64 
+dotnet publish --configuration Release -o /var/www/globe/
 sudo systemctl start kestrel-globe
